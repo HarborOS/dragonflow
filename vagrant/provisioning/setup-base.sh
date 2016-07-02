5815b9ee82243530688f0ae7fe8f1304ed59b6d7
Pete Birley <petebirley@gmail.com>
2016-07-02 23:50:42 +0100

    Update setup-base.sh

--- vagrant/provisioning/setup-base.sh
+++ vagrant/provisioning/setup-base.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo rm -rf /var/lib/apt/lists/* || true
 DEBIAN_FRONTEND=noninteractive sudo apt-get -qqy update
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy git
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy bridge-utils
@@ -6,7 +7,10 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy ebtables
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy python-pip
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy python-dev
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -qqy build-essential
+sudo pip install -U pip
 sudo pip install -U pbr
+sudo pip install -U os-testr
+
 echo export LC_ALL=en_US.UTF-8 >> ~/.bash_profile
 echo export LANG=en_US.UTF-8 >> ~/.bash_profile
 if [ ! -d "devstack" ]; then
